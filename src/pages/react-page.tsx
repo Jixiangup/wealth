dcd8676927e1902438b52414ef10c9b271d5103a
co1oxan <bnytezz@gmail.com>
2024-03-07 17:33:07 +0800

    init project

---
+++ src/pages/react-page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@theme/Layout";
+
+export default () => {
+    return (
+        <Layout title="React页面">
+            <h1>我的React页面</h1>
+            <p>这是一个React构建出来的页面</p>
+        </Layout>
+    );
+}
